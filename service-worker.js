8d56ca0e61504993a750f6d3dd81a643cb7b0781
rafif11 <rafifz666@gmail.com>
2020-07-04 22:41:37 +0700

    first pwa

---
+++ service-worker.js
@@ -0,0 +1,78 @@
+const CACHE_NAME = "firstpwa";
+var urlsToCache = [
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/pages/lore.html",
+  "/pages/profile.html",
+  "/pages/quote.html",
+  "/pages/support.html",
+  "/css/materialize.min.css",
+  "/css/style.css",
+  "/js/materialize.min.js",
+  "/js/script.js",
+  "/img/square/arthas.jpg",
+  "/img/square/illidan.jpg",
+  "/img/square/invincible.jpg",
+  "/img/square/jaina.jpg",
+  "/img/square/uther.jpg",
+  "/img/wide/arthas.jpg",
+  "/img/wide/illidan.jpg",
+  "/img/wide/invincible.jpg",
+  "/img/wide/jaina.jpg",
+  "/img/wide/uther.jpg",
+  "/img/icons/icon-72x72.png",
+  "/img/icons/icon-96x96.png",
+  "/img/icons/icon-128x128.png",
+  "/img/icons/icon-144x144.png",
+  "/img/icons/icon-152x152.png",
+  "/img/icons/icon-192x192.png",
+  "/img/icons/icon-384x384.png",
+  "/img/icons/icon-512x512.png",
+  "/manifest.json"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+      caches
+        .match(event.request, { cacheName: CACHE_NAME })
+        .then(function(response) {
+          if (response) {
+            console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+            return response;
+          }
+   
+          console.log(
+            "ServiceWorker: Memuat aset dari server: ",
+            event.request.url
+          );
+          return fetch(event.request);
+        })
+    );
+  });
+
+  self.addEventListener("activate", function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheName != CACHE_NAME) {
+              console.log("ServiceWorker: cache " + cacheName + " dihapus");
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+  
+
+
